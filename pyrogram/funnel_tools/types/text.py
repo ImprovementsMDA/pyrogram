a8a0e46eae6c84e30ede9d75176277495bf5819a
pyegork <egorkoromyslov@gmail.com>
2023-12-17 15:44:35 +0300

    added triggers-system

--- pyrogram/funnel_tools/types/text.py
+++ pyrogram/funnel_tools/types/text.py
@@ -8,9 +8,13 @@ __all__ = ["Text"]
 class Text:
     value: str | None
     count: int = field(init=False, default=0)
+    tag: str = field(default=None)
 
-    def success_sent(self):
+    def success_sent(self, **on_success_kwargs):
         self.count += 1
+        if self.tag is not None:
+            user = on_success_kwargs['user']
+            user.tags.append(self.tag)
 
     def __str__(self):
         if self.value is None:
--- pyrogram/funnel_tools/types/texts_dp.py
+++ pyrogram/funnel_tools/types/texts_dp.py
@@ -19,11 +19,16 @@ class TextsDP:
     texts: list[Text]
     dispatching_type: DispatchingTextType
 
-    def get_text(self) -> Text:
+    def get_text(self, tag: str | None = None) -> Text:
+        # Filter by tags
+        texts = [text for text in self.texts if text.tag == tag] if tag is not None else self.texts
+        if not texts:
+            raise ValueError(f"No texts with current tag: {tag=}")
+
         if self.dispatching_type == DispatchingTextType.random:
-            return random.choice(self.texts)
+            return random.choice(texts)
 
         elif self.dispatching_type == DispatchingTextType.equally:
-            return min(self.texts, key=lambda text: text.count)
+            return min(texts, key=lambda text: text.count)
 
         raise ValueError(f"Incorrect {self.dispatching_type.value=}")
