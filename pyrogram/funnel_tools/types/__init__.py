e4e815b5b907fd893e966c6ecb996064408ac4df
pyegork <egorkoromyslov@gmail.com>
2023-12-18 13:05:11 +0300

    imports fix

--- pyrogram/funnel_tools/types/__init__.py
+++ pyrogram/funnel_tools/types/__init__.py
@@ -1,7 +1,8 @@
+from .set_attrs import SetAttrs
 from .text import *
 from .texts_dp import *
 from .trigger import *
 from .message import *
 from .messages_list import *
 from .step import *
-from .set_attrs import SetAttrs
+
